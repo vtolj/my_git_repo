3e3af7b46ab6a2c4d6eb686a321ac38dfbe4178c
vtolj <vlad.tolj@gmail.com>
2020-06-12 09:24:46 +0200

    removing unnecessary files

--- container_deployment.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#Create container, first run az login for credentials 
-
-az container create --resource-group cloud-shell-storage-westeurope --name bashtestcontainer --image microsoft/aci-helloworld --ports 80 --location westeurope
-
-echo "Deploy successful!"
-
